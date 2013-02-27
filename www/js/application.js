622cef37e2fae84bcd3911bd609d2a9cfdcd851a
Angelos Lenis <anglen@ebs.gr>
2013-02-27 16:28:35 +0200

    Fixed a bug in validation extension "requiredIfOtherGreek"

--- www/js/application.js
+++ www/js/application.js
@@ -14,7 +14,7 @@ define([ "jquery", "underscore", "backbone", "bootstrap", "jquery.ui", "jquery.i
 					$(element).valid();
 				});
 			}
-			return (/^[a-zA-Z0-9]*$/.test(target.val()));
+			return (/^[a-zA-Z0-9]*$/.test(target.val())) || value;
 		}, "Required if other field is in greek characters");
 
 		$.validator.addMethod("pwd", function(value, element) {
@@ -34,63 +34,58 @@ define([ "jquery", "underscore", "backbone", "bootstrap", "jquery.ui", "jquery.i
 			dateFormat : "dd/mm/yy"
 		});
 		// DataTables Plugin Extensions/Configurations
-		
+
 		/* Default class modification */
-		$.extend( $.fn.dataTableExt.oStdClasses, {
-			"sWrapper": "dataTables_wrapper form-inline"
-		} );
+		$.extend($.fn.dataTableExt.oStdClasses, {
+			"sWrapper" : "dataTables_wrapper form-inline"
+		});
 		/* API method to get paging information */
-		$.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
-		{
+		$.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings) {
 			return {
-				"iStart":         oSettings._iDisplayStart,
-				"iEnd":           oSettings.fnDisplayEnd(),
-				"iLength":        oSettings._iDisplayLength,
-				"iTotal":         oSettings.fnRecordsTotal(),
-				"iFilteredTotal": oSettings.fnRecordsDisplay(),
-				"iPage":          oSettings._iDisplayLength === -1 ?
-					0 : Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
-				"iTotalPages":    oSettings._iDisplayLength === -1 ?
-					0 : Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
+				"iStart" : oSettings._iDisplayStart,
+				"iEnd" : oSettings.fnDisplayEnd(),
+				"iLength" : oSettings._iDisplayLength,
+				"iTotal" : oSettings.fnRecordsTotal(),
+				"iFilteredTotal" : oSettings.fnRecordsDisplay(),
+				"iPage" : oSettings._iDisplayLength === -1 ? 0 : Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+				"iTotalPages" : oSettings._iDisplayLength === -1 ? 0 : Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
 			};
 		};
 		/* Bootstrap style pagination control */
-		$.extend( $.fn.dataTableExt.oPagination, {
-			"bootstrap": {
-				"fnInit": function( oSettings, nPaging, fnDraw ) {
+		$.extend($.fn.dataTableExt.oPagination, {
+			"bootstrap" : {
+				"fnInit" : function(oSettings, nPaging, fnDraw) {
 					var oLang = oSettings.oLanguage.oPaginate;
-					var fnClickHandler = function ( e ) {
+					var fnClickHandler = function(e) {
 						e.preventDefault();
-						if ( oSettings.oApi._fnPageChange(oSettings, e.data.action) ) {
-							fnDraw( oSettings );
+						if (oSettings.oApi._fnPageChange(oSettings, e.data.action)) {
+							fnDraw(oSettings);
 						}
 					};
 
-					$(nPaging).addClass('pagination').append(
-						'<ul>'+
-							'<li class="prev disabled"><a href="#">&larr; '+oLang.sPrevious+'</a></li>'+
-							'<li class="next disabled"><a href="#">'+oLang.sNext+' &rarr; </a></li>'+
-						'</ul>'
-					);
+					$(nPaging).addClass('pagination').append('<ul>' + '<li class="prev disabled"><a href="#">&larr; ' + oLang.sPrevious + '</a></li>' + '<li class="next disabled"><a href="#">' + oLang.sNext + ' &rarr; </a></li>' + '</ul>');
 					var els = $('a', nPaging);
-					$(els[0]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
-					$(els[1]).bind( 'click.DT', { action: "next" }, fnClickHandler );
+					$(els[0]).bind('click.DT', {
+						action : "previous"
+					}, fnClickHandler);
+					$(els[1]).bind('click.DT', {
+						action : "next"
+					}, fnClickHandler);
 				},
 
-				"fnUpdate": function ( oSettings, fnDraw ) {
+				"fnUpdate" : function(oSettings, fnDraw) {
 					var iListLength = 5;
 					var oPaging = oSettings.oInstance.fnPagingInfo();
 					var an = oSettings.aanFeatures.p;
-					var i, ien, j, sClass, iStart, iEnd, iHalf=Math.floor(iListLength/2);
+					var i, ien, j, sClass, iStart, iEnd, iHalf = Math.floor(iListLength / 2);
 
-					if ( oPaging.iTotalPages < iListLength) {
+					if (oPaging.iTotalPages < iListLength) {
 						iStart = 1;
 						iEnd = oPaging.iTotalPages;
-					}
-					else if ( oPaging.iPage <= iHalf ) {
+					} else if (oPaging.iPage <= iHalf) {
 						iStart = 1;
 						iEnd = iListLength;
-					} else if ( oPaging.iPage >= (oPaging.iTotalPages-iHalf) ) {
+					} else if (oPaging.iPage >= (oPaging.iTotalPages - iHalf)) {
 						iStart = oPaging.iTotalPages - iListLength + 1;
 						iEnd = oPaging.iTotalPages;
 					} else {
@@ -98,30 +93,29 @@ define([ "jquery", "underscore", "backbone", "bootstrap", "jquery.ui", "jquery.i
 						iEnd = iStart + iListLength - 1;
 					}
 
-					for ( i=0, ien=an.length ; i<ien ; i++ ) {
+					for (i = 0, ien = an.length; i < ien; i++) {
 						// Remove the middle elements
 						$('li:gt(0)', an[i]).filter(':not(:last)').remove();
 
 						// Add the new list items and their event handlers
-						for ( j=iStart ; j<=iEnd ; j++ ) {
-							sClass = (j==oPaging.iPage+1) ? 'class="active"' : '';
-							$('<li '+sClass+'><a href="#">'+j+'</a></li>')
-								.insertBefore( $('li:last', an[i])[0] )
-								.bind('click', function (e) {
-									e.preventDefault();
-									oSettings._iDisplayStart = (parseInt($('a', this).text(),10)-1) * oPaging.iLength;
-									fnDraw( oSettings );
-								} );
+						for (j = iStart; j <= iEnd; j++) {
+							sClass = (j == oPaging.iPage + 1) ? 'class="active"' : '';
+							$('<li ' + sClass + '><a href="#">' + j + '</a></li>').insertBefore($('li:last', an[i])[0]).bind('click', function(e) {
+								e.preventDefault();
+								oSettings._iDisplayStart = (parseInt($('a', this).text(), 10) - 1) * oPaging.iLength;
+								fnDraw(oSettings);
+							});
 						}
 
-						// Add / remove disabled classes from the static elements
-						if ( oPaging.iPage === 0 ) {
+						// Add / remove disabled classes from the static
+						// elements
+						if (oPaging.iPage === 0) {
 							$('li:first', an[i]).addClass('disabled');
 						} else {
 							$('li:first', an[i]).removeClass('disabled');
 						}
 
-						if ( oPaging.iPage === oPaging.iTotalPages-1 || oPaging.iTotalPages === 0 ) {
+						if (oPaging.iPage === oPaging.iTotalPages - 1 || oPaging.iTotalPages === 0) {
 							$('li:last', an[i]).addClass('disabled');
 						} else {
 							$('li:last', an[i]).removeClass('disabled');
@@ -129,7 +123,7 @@ define([ "jquery", "underscore", "backbone", "bootstrap", "jquery.ui", "jquery.i
 					}
 				}
 			}
-		} );
+		});
 		/* Custom Add <tr> function */
 		$.fn.dataTableExt.oApi.fnAddTr = function(oSettings, nTrHTML) {
 			if (typeof bRedraw == 'undefined') {
